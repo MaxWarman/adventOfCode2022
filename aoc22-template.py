39b494910df31dd4d5899c47ce2414813ed6137b
MaxWarman <maksymilian_gorski@wp.pl>
2022-12-05 23:32:17 +0100

    Removed template from .gitignore

---
+++ aoc22-template.py
@@ -0,0 +1,38 @@
+"""
+Author: github.com/MaxWarman
+Advent of Code 2022 - Day X
+"""
+
+def getInputLines(inputPath):
+    f = open(inputPath, "rt")
+    inputLines = f.readlines()
+    f.close()
+
+    for i in range(len(inputLines)):
+        inputLines[i] = inputLines[i].replace("\n", "")
+
+    return inputLines
+
+def solvePartOne(inputLines):
+    pass
+    
+    #solution = None
+    #print(f"Day X, part 1 solution: {solution}")
+
+def solvePartTwo(inputLines):
+    pass
+    
+    #solution = None
+    #print(f"Day X, part 2 solution: {solution}")
+
+def main():
+    inptuPath = "dayX_input.txt"
+
+    inputLines = getInputLines(inptuPath)
+
+    solvePartOne(inputLines)
+    solvePartTwo(inputLines) 
+
+
+if __name__ == "__main__":
+    main()
